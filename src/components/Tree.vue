5247394c6712e591655e9e97f14dfe0cbdf76e7d
mindaugas-pro <>
2020-11-22 20:01:39 +0200

    new component created

---
+++ src/components/Tree.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>{{ this.node.name }}</h1>
+    <Tree v-for="child in node.children" :key="child.name" :node="child">
+      {{ child.name }}
+    </Tree>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Tree",
+  props: {
+    node: Object
+  },
+  data() {
+    return {
+      // node: Object
+    };
+  },
+  computed: {
+    root: {
+      get() {
+        return this.$store.getters.getRoot;
+      }
+    },
+    // node: {
+    //   get() {
+    //     return this.$store.getters.getNode;
+    //   },
+    //   set(value) {
+    //     this.$store.commit("SET_NODE", value);
+    //   }
+    // },
+    ...mapGetters(["getRoot"])
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
