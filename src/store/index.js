98e8c15c8c793a63a393c05164a608419951687a
mindaugas-pro <>
2020-11-26 12:28:15 +0200

    var, setter, getter created

--- src/store/index.js
+++ src/store/index.js
@@ -8,14 +8,17 @@ export default new Vuex.Store({
     root: {
       name: "/",
       id: 0,
+      rootId: 0,
       children: [
         {
           name: "folder 1",
           id: 1,
+          rootId: 1,
           children: [
             {
               name: "folder 2",
               id: 2,
+              rootId: 1,
               children: []
             }
           ]
@@ -23,10 +26,12 @@ export default new Vuex.Store({
         {
           name: "folder 3",
           id: 3,
+          rootId: 2,
           children: [
             {
               name: "folder 4",
               id: 4,
+              rootId: 2,
               children: []
             }
           ]
@@ -34,6 +39,7 @@ export default new Vuex.Store({
       ]
     },
     folderCount: 4,
+    rootId: 2,
     draggable: Object,
     draggableParent: Object
   },
@@ -49,6 +55,9 @@ export default new Vuex.Store({
     },
     SET_DRAGGABLE_PARENT(state, value) {
       state.draggableParent = value;
+    },
+    SET_ROOT_ID(state, value) {
+      state.rootId = value;
     }
   },
   getters: {
@@ -63,6 +72,9 @@ export default new Vuex.Store({
     },
     getDraggableParent: state => {
       return state.draggableParent;
+    },
+    getRootId: state => {
+      return state.rootId;
     }
   },
   actions: {},
